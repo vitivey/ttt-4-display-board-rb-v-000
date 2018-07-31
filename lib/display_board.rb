7362b2f9a49e0264d9cfe604e23fd8d1938e26a9
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-07-31 03:41:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,14 @@
 # out the current state.
 board=["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 def display_board(board)
-  row=["   ","|","   "   "|"   "]
+    row_1=["   |   |   "]
+    row_2=["   |   |   "]
+    row_3=["   |   |   "]
+    separator=[-----------]
+    puts row_1
+    puts separator
+    puts row_2
+    puts separator
+    puts row_3
 end
+display_board
