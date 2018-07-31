d2922d7ad70af0d8bfdc76d48b78d2b8ac4ffd0d
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-07-31 03:42:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board=["   ","   ","   ","   ","   ","   ","   ","   ","   "]
-def display_board(board)
+def display_board
     row_1=["   |   |   "]
     row_2=["   |   |   "]
     row_3=["   |   |   "]
