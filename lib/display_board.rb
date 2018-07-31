2f6e35679e830880b8816a157e8ae854e216b970
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-07-31 03:56:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,11 +5,11 @@ def display_board(board)
     row_1 = [" #{board[0]} | #{board[1]} | #{board[2]} "]
     row_2 = [" #{board[3]} | #{board[4]} | #{board[5]} "]
     row_3 = [" #{board[6]} | #{board[7]} | #{board[8]} "]
-    separator=["-----------"]
+    separator = ["-----------"]
     puts row_1
     puts separator
     puts row_2
     puts separator
     puts row_3
 end
-puts display_board
+puts display_board(board)
